b0fe13e15820e136589febed73e37ad8a4b95706
milimlee <milimlee0@gmail.com>
2019-01-14 18:58:52 -0500

    class, object and method of C#

---
+++ Student/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student p = new Student("Fred", 1, "Programming");
+            Student p2 = new Student();
+            p2.setName("Greg");
+            p2.setId(2);
+            p2.setProgram("Art");
+
+            Console.WriteLine(p.getName() + p.getProgram() + p.getId());
+            Console.WriteLine(" ");
+
+            Student2 s = new Student2();
+
+            s.Program = "Business";
+            s.Id = 3;
+            s.Name = "Ram";
+
+            Console.WriteLine(s.Name + " " + s.Id + " " + s.Program);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Student/Student.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentEx
+{
+    class Student
+    {
+        private string name;
+        private int id;
+        private string program;
+
+        public Student (string n, int i, string p)
+        {
+            name = n;
+            id = i;
+            program = p;
+        }
+
+        public Student()
+        {
+           
+        }
+
+        public void setName(string n)
+        {
+            name = n;
+        }
+
+        public void setProgram(string p)
+        {
+            program = p;
+        }
+
+        public void setId(int i)
+        {
+            id = i;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public string getProgram()
+        {
+            return program;
+        }
+
+        public int getId()
+        {
+            return id;
+        }
+
+    }
+
+    class Student2
+    {
+        // Properties -> an extension of fields 
+        // variables that are accessed like methods
+        // 차이점 알아야 함
+        public string Name
+        {
+            get;set;
+        }
+
+        public string Program
+        {
+            get;set;
+        }
+
+        public int Id
+        {
+            get;set;
+        }
+    }
+}
