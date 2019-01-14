187b458314095cadc7d924a73a64070d78e10334
milimlee <milimlee0@gmail.com>
2019-01-14 18:56:49 -0500

    Shared and Client Assembly

---
+++ Assembly/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharedAssemblyEx;
+
+namespace ClientforSharedAssembly
+{
+    public class Class1
+    {
+        public static void Main(string[] arv)
+        {
+            string uname;
+            Console.Write("Enter your name! ");
+            uname = Console.ReadLine();
+            SharedAssemblyClass SharedAssemblyClass = new SharedAssemblyClass();
+            Console.WriteLine(SharedAssemblyClass.WelcomeMsg(uname));
+            Console.Read();
+        }
+    }
+}
