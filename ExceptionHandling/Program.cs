48f69d0fa5622ca7eaee8444cceec79773990472
milimlee <milimlee0@gmail.com>
2019-01-28 10:45:41 -0500

    BasicExceptionHandling

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+namespace BasicExceptionHandling
+{
+
+    class DivNumbers
+    {
+        int result;
+        public DivNumbers()
+        {
+            result = 0;
+        }
+        public void division(int num, int num2)
+        {
+            try
+            {
+                result = num / num2; // what if denomitator = 0
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Exception error {0}", e);
+                // CWL on quiz/test
+            }
+            finally
+            {
+                Console.WriteLine("Result {0} ", result);
+            }
+
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DivNumbers d = new DivNumbers(); // 미드텀에 나옴 설명할수 있어야 함
+            d.division(10, 0);
+            Console.ReadKey();
+            // HOMEWORK: modify the prog so that the user provide the input
+            // modify the code so that the prog will continually allow
+            // as long as the user provides a value of 0 for the denominator
+        }
+    }
+}
+*/
