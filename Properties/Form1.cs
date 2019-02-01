9f8cce48a9a45324a936a76109066f07a9694c49
milimlee <milimlee0@gmail.com>
2019-01-31 19:46:21 -0500

    Add files via upload

---
+++ Properties/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EX08
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Customer cust = new Customer("Ian", "Mora", 20);
+            cust.Address = "444 Karr St, Oakville";
+
+            //CustFullname.Text = cust.GetFullname();
+            CustFullname.Text = cust.Fullname;
+
+            //CustLastname.Text = cust.Lastname;
+            // CustAge.Text = cust.GetAge().ToString();
+            CustAge.Text = cust.Age.ToString();
+            CustAddress.Text = cust.Address;
+            CustDescription.Text = cust.Description;
+
+            //bool test = cust.getIsQualified();
+            bool test = cust.IsQualified;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CustFullname_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
