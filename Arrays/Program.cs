63f5dc5b72bdbf3fdafa0757c8a9a144cb1099cb
milimlee <milimlee0@gmail.com>
2019-02-10 01:11:31 -0500

    Add files via upload

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -4,44 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Jagged_Arrays
+namespace JaggedArrayEx2
 {
-
     class Program
     {
-
         static void Main(string[] args)
         {
-            Random rand = new Random();
-            int size = 8;
-
-            int[][] G = new int[size][]; // format
-            for (int i = 0; i < size; i++)
+            int[][] jagged = //new int[][]- 가능
             {
-                int[] sub = new int[rand.Next(1, 20)]; //HW== what this line does
-                // int[] sub = new int[4];
+                new int[] { 1, 0, 2, 4, 6},
+                new int[] { 1, 6 },
+                new int[] { 1, 0, 2, 4, 6, 45, 67, 78 },
+                new int[] { 3, 5, 7 },
+            };
 
-                for (int j = 0; j < sub.Length ; j++)
+            for (int i = 0; i < jagged.GetLength(0); i++)
+            {
+                for (int j = 0; j < jagged[i].Length; j++)
                 {
-                    sub[j] = rand.Next(20, 100);
+                    Console.Write(jagged[i][j] + " ");
+                    
                 }
-                G[i] = sub; // HW == what this line does
-            }
-
-
-            for (int i = 0; i < size; i++)
-            {               
-
-                for (int j = 0; j < G[i].Length; j++) // test에 이걸 응용해서 나온다-- 반드시 이해해야함채ㅡcompare 23, 34
-                {
-                    Console.Write(G[i][j] + " ");
-                }
-
-                Console.WriteLine(" "); 
+                Console.WriteLine();
             }
 
             Console.Read();
-
         }
     }
 }
